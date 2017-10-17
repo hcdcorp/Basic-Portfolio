dabd49772e7274e509c19973438ebe6b1798561f
hcdcorp <hcdcorporation@gmail.com>
2017-10-17 19:40:50 -0400

    give me friends dude

---
+++ Downloads/BLK Hack/CWRU/friend-finder-master/Data/friends.js
@@ -0,0 +1,41 @@
+var friends = [
+   {
+      "name":"Assata Shakur",
+      "photo":"http://www.blackenterprise.com/wp-content/blogs.dir/1/files/2015/10/assatashakur.jpg",
+      "scores":[5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+      "name":"Kwame Ture",
+      "photo":"http://www.caribbeanmemoryproject.com/uploads/3/9/6/8/39688192/258132176_orig.jpg",
+      "scores":[4,2,5,1,3,2,2,1,3,1]
+   },
+   {
+      "name":"El Hajj Malik El Shabazz",
+      "photo":"http://vc-xcenter.s3.amazonaws.com/files/2014-02-05-10/1198malcolmxelhajjmalikshabazzweb.jpg",
+      "scores":[5,2,2,2,4,1,3,2,5,1]
+   },
+   {
+      "name":"Chairman Fred Hampton",
+      "photo":"https://upload.wikimedia.org/wikipedia/en/9/94/Fred_Hampton.jpg",
+      "scores":[3,3,4,2,2,1,3,2,2,1]
+   },
+	{
+	   "name":"Nikki Giovanni",
+	   "photo":"https://wellreadblackgirl.com/wp-content/uploads/2016/08/giovanni_nikki.jpg",
+	   "scores":[4,3,4,1,5,2,5,3,1,1]
+	},
+      {
+      "name":"James Baldwin",
+      "photo":"http://www.post-gazette.com/image/2017/02/17/1140x_q90_a10-7_cTC_ca386,0,909,389/baldwin-1223.jpg",
+      "scores":[3,3,4,1,4,3,5,3,5,1]
+   },
+	{
+	   "name":"Phylicia Rashad",
+	   "photo":"http://static.playbill.com/dims4/default/614b406/2147483647/resize/250x/quality/90/?url=http%3A%2F%2Fstatic.playbill.com%2F7d%2Fa2%2Fa0b17a674f61a6003bc52171b974%2FPhylicia-Rashad_1414000014.jpg",
+	   "scores":[4,4,2,3,2,2,3,2,4,1]
+	}
+
+]
+
+module.exports = friends;
+
